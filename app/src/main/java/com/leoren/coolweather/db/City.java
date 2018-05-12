1fa257ac9e9efee390d57f1e087ec2f1acc2659d
Leoren <3290183219@qq.com>
2018-05-12 08:37:32 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/leoren/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.leoren.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int provinceId;
+
+    private int CityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getCityCode() {
+        return CityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        CityCode = cityCode;
+    }
+}
---
+++ app/src/main/java/com/leoren/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.leoren.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private int cityId;
+
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWertherId() {
+        return weatherId;
+    }
+
+    public void setWertherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
